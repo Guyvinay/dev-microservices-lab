e92352ea6c03df19c6b18ea61f32c00593e3872a
Vinay Kumar Singh <vinay.singh@prospecta.com>
2024-06-16 21:56:10 +0530

    rabbitMq starter project

---
+++ backend/rabbit-starter/src/main/java/com/pros/RabbitStarterApplication.java
@@ -0,0 +1,13 @@
+package com.pros;
+
+import org.springframework.context.annotation.Configuration;
+
+//@SpringBootApplication
+@Configuration
+public class RabbitStarterApplication {
+
+//	public static void main(String[] args) {
+//		SpringApplication.run(RabbitStarterApplication.class, args);
+//	}
+
+}
