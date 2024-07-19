b3c9f5bbc3e1fa04a74e8563061f42e55baab9c8
Vinay Kumar Singh <vinay.singh@prospecta.com>
2024-07-19 11:23:01 +0530

    grpc implemented

--- backend/pros-revised/src/main/java/com/dev/grpc/GrpcServerImpl.java
+++ backend/pros-revised/src/main/java/com/dev/grpc/GrpcServerImpl.java
@@ -1,9 +1,23 @@
 package com.dev.grpc;
 
+import com.dev.User;
+import com.dev.UserDetail;
 import com.dev.UserServiceGrpc;
+import io.grpc.stub.ServerCalls;
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
 
 @GrpcService
+@Slf4j
 public class GrpcServerImpl extends UserServiceGrpc.UserServiceImplBase {
-    
+
+    @Override
+    public void createUser(UserDetail request, StreamObserver<User> responseObserver) {
+
+        log.info("request received {}", request);
+        User user = User.newBuilder().setName("Vinay").build();
+        responseObserver.onNext(user);
+        responseObserver.onCompleted();
+    }
 }
