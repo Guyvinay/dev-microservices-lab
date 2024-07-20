3200e38c04fa84800e3aee46dc5c852404a63cd8
Vinay Kumar Singh <mrsinghvinay563@gmail.com>
2024-07-20 13:31:15 +0530

    grpc

--- backend/pros-revised/src/main/java/com/dev/grpc/DocumentService.java
+++ backend/pros-revised/src/main/java/com/dev/grpc/DocumentService.java
@@ -4,10 +4,14 @@ import com.dev.common.dto.document.Document;
 import com.dev.service.ElasticService;
 import io.grpc.stub.ServerCalls;
 import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
+@GrpcService
+@Slf4j
 public class DocumentService extends DocumentServiceGrpc.DocumentServiceImplBase {
 
     @Autowired
@@ -16,10 +20,12 @@ public class DocumentService extends DocumentServiceGrpc.DocumentServiceImplBase
     @Override
     public void getAllDocuments(Empty request, StreamObserver<DocumentsResponse> responseObserver) {
         List<Document> documents =  elasticService.getAllDocumentsFromElastic();
+        log.info("documents [{}]", documents);
         DocumentsResponse.newBuilder();
         DocumentsResponse.Builder docs = DocumentsResponse.newBuilder();
-        docs.addAllDocuments(documents);
+//        docs.addAllDocuments(documents);
         responseObserver.onNext(docs.build());
+        responseObserver.onCompleted();
     }
 
 }
