a7f3e14f7f8d8820bb6f9432b53a190386f880c6
Vinay Kumar Singh <vinay.singh@prospecta.com>
2024-08-08 11:34:01 +0530

    implementing-ngrx-store

---
+++ frontend/dev-ng/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
+
+
+/*
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://github.com/ngrx/platform
+*/
