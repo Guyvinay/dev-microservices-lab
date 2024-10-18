c08cef87eac61b45840e9f891516ea1add9a52cf
Vinay Kumar Singh <vinay.singh@prospecta.com>
2024-10-18 22:01:27 +0530

    chart js integration

---
+++ frontend/dev-ng/src/app/_modules/home/_components/chart-js/chart.js.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'chart-js',
+    templateUrl: 'chart.js.component.html',
+    styleUrls:['chart.js.component.scss']
+})
+export class ChartJsComponent  implements OnInit{
+
+    ngOnInit(): void {
+    }
+}
--- frontend/dev-ng/src/app/app.module.ts
+++ frontend/dev-ng/src/app/app.module.ts
@@ -1,47 +1,52 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import {StoreModule} from '@ngrx/store'
-import { AppComponent } from './app.component';
-import { counterReducer } from './store/state/counter.reducer';
-import { CounterComponent } from './counter/counter.component';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools'
-import { PostComponent } from './posts/post.component';
-import { EffectsModule } from '@ngrx/effects';
-import { PostEffects } from './store/effects/post.effects';
-import { postsReducer } from './store/state/posts.reducer';
-import { HttpClientModule } from '@angular/common/http';
-import { RxjsComponent } from './_modules/home/_components/rxjs/rxjs.component';
-import { MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgMaterialComponent } from './_modules/home/_components/ng-material/ng-material.component';
-import { MatTableModule } from '@angular/material/table'; // Import MatTableModule
-import { MatPaginatorModule} from '@angular/material/paginator';
-import {MatTabsModule} from '@angular/material/tabs';
-import { MatMenuModule } from '@angular/material/menu';
-
+import { StoreModule } from "@ngrx/store";
+import { AppComponent } from "./app.component";
+import { counterReducer } from "./store/state/counter.reducer";
+import { CounterComponent } from "./counter/counter.component";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { PostComponent } from "./posts/post.component";
+import { EffectsModule } from "@ngrx/effects";
+import { PostEffects } from "./store/effects/post.effects";
+import { postsReducer } from "./store/state/posts.reducer";
+import { HttpClientModule } from "@angular/common/http";
+import { RxjsComponent } from "./_modules/home/_components/rxjs/rxjs.component";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgMaterialComponent } from "./_modules/home/_components/ng-material/ng-material.component";
+import { MatTableModule } from "@angular/material/table"; // Import MatTableModule
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatMenuModule } from "@angular/material/menu";
+import { ChartJsComponent } from "./_modules/home/_components/chart-js/chart.js.component";
 
 @NgModule({
-  declarations: [AppComponent, CounterComponent, PostComponent, RxjsComponent, NgMaterialComponent],
+  declarations: [
+    AppComponent,
+    CounterComponent,
+    PostComponent,
+    RxjsComponent,
+    NgMaterialComponent,
+    ChartJsComponent,
+  ],
   imports: [
     BrowserModule,
     MatProgressSpinnerModule,
     BrowserAnimationsModule,
-    MatTableModule,           // Add MatTableModule here
+    MatTableModule, // Add MatTableModule here
     MatTabsModule,
     MatPaginatorModule,
     MatMenuModule,
     HttpClientModule,
-    StoreModule.forRoot({count: counterReducer, posts: postsReducer}),
+    StoreModule.forRoot({ count: counterReducer, posts: postsReducer }),
     EffectsModule.forRoot([PostEffects]),
-    StoreDevtoolsModule.instrument({maxAge: 25}),
+    StoreDevtoolsModule.instrument({ maxAge: 25 }),
   ],
-  providers: [
-  ],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
-
 
 /*
 Use of this source code is governed by an MIT-style license that
