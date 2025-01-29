63ffce12e17d58bb670511249260edfc7eae19d6
Vinay Singh <vinay.singh@prospecta.com>
2025-01-29 09:05:23 +0530

    alteration in security, need to get more

---
+++ backend/dev-auth/src/main/java/com/dev/auth/service/impl/AuthServiceImpl.java
@@ -0,0 +1,34 @@
+package com.dev.auth.service.impl;
+
+import com.dev.auth.dto.LoginRequestDTO;
+import com.dev.auth.security.details.CustomAuthToken;
+import com.dev.auth.service.AuthService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthServiceImpl(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    /**
+     * @param loginRequestDTO 
+     * @return
+     */
+    @Override
+    public String login(LoginRequestDTO loginRequestDTO) {
+        Authentication authentication = new CustomAuthToken(
+                loginRequestDTO.getOrgId(),
+                loginRequestDTO.getUsername(),
+                loginRequestDTO.getPassword()
+        );
+        Authentication authenticated = authenticationManager.authenticate(authentication);
+
+        return "login successful";
+    }
+}
