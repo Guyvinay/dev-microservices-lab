045f9951392c31150473c1058052cc98ffeafe42
guyvinay <mrsinghvinay563@gmial.com>
2025-09-06 23:47:08 +0530

    refactored

--- backend/dev-sandbox/src/main/java/com/dev/controller/ProfileController.java
+++ backend/dev-sandbox/src/main/java/com/dev/controller/ProfileController.java
@@ -25,32 +25,6 @@ public class ProfileController {
     @Autowired
     private ProfileService profileService;
 
-
-
-    @PostMapping
-    public ResponseEntity<Profile> saveProfiles(@RequestBody Profile profile) throws IOException {
-
-        String successPayload = new String(Files.readAllBytes(Paths.get("payloads/DqPayloadSuccess.json").toAbsolutePath()));
-
-        JSONObject jsonObject = new JSONObject(successPayload);
-        String automationNumber = UUID.randomUUID().toString();
-
-            JSONArray vcArray = jsonObject.getJSONObject("mdoRecordES")
-                    .getJSONObject("hdvs")
-                    .getJSONObject("FLD_545886285")
-                    .getJSONArray("vc");
-
-
-
-            JSONObject firstVcObject = vcArray.getJSONObject(0);
-            firstVcObject.put("c", automationNumber);
-
-            String jsonBody = jsonObject.toString();
-        //   System.out.println("Request JSON... " + jsonBody + "\n");
-        log.info("profile {}", jsonBody);
-        return new ResponseEntity<Profile>(profile, HttpStatus.ACCEPTED);
-    }
-
     @GetMapping(value = "/{id}")
     public ResponseEntity<Profile> getProfileById(@PathVariable("id") String id) throws IOException {
         return new ResponseEntity<Profile>(profileService.findById(id), HttpStatus.ACCEPTED);
