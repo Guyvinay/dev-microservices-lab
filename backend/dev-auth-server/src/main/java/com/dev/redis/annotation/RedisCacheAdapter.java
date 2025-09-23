157aa3ec23dd37f65ec442055122d45dd36fe450
guyvinay <mrsinghvinay563@gmial.com>
2025-09-23 20:50:43 +0530

    redis aspect implementation

---
+++ backend/dev-auth-server/src/main/java/com/dev/redis/annotation/RedisCacheAdapter.java
@@ -0,0 +1,28 @@
+package com.dev.redis.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(value = {ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface RedisCacheAdapter {
+
+    String name() default "";
+
+    long ttl() default 20;
+
+    boolean log() default true;
+
+    RedisCacheOperation operation() default RedisCacheOperation.CACHE;
+
+    boolean allEntries() default false;
+
+    enum RedisCacheOperation {
+        CACHE, PUT, EVICT
+    }
+
+}
