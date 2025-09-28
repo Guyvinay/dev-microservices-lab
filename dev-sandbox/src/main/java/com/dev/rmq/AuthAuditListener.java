a4818463877764e5dce458f3f492f9e5af8922ca
guyvinay <mrsinghvinay563@gmial.com>
2025-09-28 18:00:32 +0530

    schema creation on receiving tenant from auth

--- dev-sandbox/src/main/java/com/dev/rmq/AuthAuditListener.java
+++ dev-sandbox/src/main/java/com/dev/rmq/AuthAuditListener.java
@@ -1,9 +1,15 @@
 package com.dev.rmq;
 
+import com.dev.hibernate.SchemaInitializer;
 import com.dev.rabbitmq.annotation.TenantRabbitListener;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
+
+import java.io.IOException;
 
 @Slf4j
 @TenantRabbitListener(
@@ -13,9 +19,20 @@ import org.springframework.amqp.core.MessageListener;
     routingKey = "dev.tenant.route",
     type = "topic"
 )
+@RequiredArgsConstructor
 public class AuthAuditListener implements MessageListener {
+
+    private final SchemaInitializer schemaInitializer;
+    private final ObjectMapper objectMapper;
     @Override
     public void onMessage(Message message) {
-        log.info("Sandbox received: {}", new String(message.getBody()));
+        try {
+            String tenantId = objectMapper.readValue(message.getBody(), new TypeReference<String>() {});
+            log.info("Sandbox received: {}", tenantId);
+            schemaInitializer.initialize(tenantId);
+        } catch (IOException e) {
+            log.error("Exception encountered: ", e);
+            throw new RuntimeException(e);
+        }
     }
 }
